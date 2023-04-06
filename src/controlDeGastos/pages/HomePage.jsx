8557c86efb63e61d04272a8012e6ecbe4104d3d7
nico1216 <nicogonzalezraggi123@gmail.com>
2023-04-06 18:55:59 -0300

    guardados

--- src/controlDeGastos/pages/HomePage.jsx
+++ src/controlDeGastos/pages/HomePage.jsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react"
 import { Link, Navigate } from "react-router-dom"
 import { Navbar } from "../components"
+import { useDispatch } from "react-redux"
 
 
 export const HomePage=()=>{
 
-    useEffect(()=>{
+    const dispatch =useDispatch()
 
-    },[])
-
-
+    dispatch()
     return(
+
+        
         <>
 
         <Navbar/>
